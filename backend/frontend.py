21be51ae35a87e8fcb4b6f1f726b256524b8fa94
jaehyeon-git <sd205@korea.ac.kr>
2021-12-12 17:56:47 +0000

    Add init.py fronted.py server.py

--- backend/frontend.py
+++ backend/frontend.py
@@ -1,4 +1,4 @@
-from turtle import width
+from turtle import back, width
 import streamlit as st
 
 # from stqdm import stqdm
@@ -11,6 +11,8 @@ import tempfile
 import logging
 import json
 import os
+import sys
+sys.path.append(os.getcwd())
 import utils.SessionState as SessionState
 from random import randint
 from utils.prototype import lookup_checkpoint_files
@@ -42,7 +44,7 @@ def trigger_rerun():
 
 def detect_image(data, server, width, height, conf, ckpt_file):
     m = MultipartEncoder(fields={"file": ("filename", data, "image/jpeg")})
-
+    print(m)
     resp = requests.post(
         server + f"/detection/image/?width={width}&height={height}&conf={conf}&ckpt_file={ckpt_file}",
         data=m,
@@ -52,11 +54,13 @@ def detect_image(data, server, width, height, conf, ckpt_file):
 
     return resp
 
-
-def detect_video(server):
-
+    
+def detect_video(data, server, width, height, conf, ckpt_file):
+    m = MultipartEncoder(fields={"file": ("filename", data, "video/mp4")})
     requests.post(
-        server + "/detection/video",
+        server + f"/detection/video/?width={width}&height={height}&conf={conf}&ckpt_file={ckpt_file}",
+        data = m,
+        headers = {"Content-Type": m.content_type},
         timeout=8000,
     )
 
@@ -75,14 +79,13 @@ def main():
     page_icon=":scooter:")
 
     state = SessionState.get(upload_key = None, enabled = True, start = False, conf = 70, nms = 50, run = False)
-
+    data_type = st.selectbox("Choose Data Type", ["Image", "Video"])
     upload = st.empty()
     start_button = st.empty()
     stop_button = st.empty()
 
 
     with upload:
-        data_type = st.selectbox("Choose Data Type", ["Image", "Video"])
         input_data = st.file_uploader('Upload Image or Video file', key = state.upload_key)
         time.sleep(1)
         
@@ -102,11 +105,12 @@ def main():
         
         result_resolution = st.sidebar.radio(
             "select result video resolution",
-            ("1280 x 960",)
+            ("1280 x 960", "640 x 480")
         )
 
         if result_resolution == "1280 x 960":
             width, height = 1280, 960
+            
         elif result_resolution == "640 x 480":
             width, height = 640, 480
     
@@ -115,18 +119,6 @@ def main():
 
     if input_data is not None:
         
-        # tfile = tempfile.NamedTemporaryFile(delete = False)
-        # tfile.write(f.read())  
-        # upload.empty()
-        # if f.type.split('/')[0].lower() == 'image':
-        #     #vf = Image.open(tfile.name)
-        #     #st.image(vf)
-        #     files = [('files', (tfile.name, tfile.type))]
-        #     response = requests.post("http://localhost:8001/detection/image", files=files)
-        # else:
-        #     vf = cv2.VideoCapture(tfile.name)
-        # print(type(vf))
-
         if not state.run:
             start = start_button.button("start")
             state.start = start
@@ -148,90 +140,18 @@ def main():
                 if data_type == "Image":
                     resp = detect_image(input_data, backend, width, height, confidence_threshold, ckpt_file)
                     st.image(resp.content)
-                input_data.close()
+                    
+                elif data_type == "Video":
+                    detect_video(input_data, backend, width, height, confidence_threshold, ckpt_file)
+                    with st.spinner(text="Detecting Finished! Converting Video Codec..."):
+                        os.system("ffmpeg -i /opt/ml/final_project/backend/result.mp4 -vcodec libx264 /opt/ml/final_project/backend/result_h264.mp4")
+                        video_file = open("/opt/ml/final_project/backend/result_h264.mp4", 'rb')
+                        video_bytes = video_file.read()
+                        st.video(video_bytes)
+                #input_data.close()
             else:
                 state.run = True
                 trigger_rerun()
-
-
-def run_app():
-
-    data_type = st.selectbox("Choose Data Type", ["Image", "Video"])
-    input_data = st.file_uploader(f"insert {data_type}")  # image upload widget
-    time.sleep(1)
-
-    if st.button("Detect Plant Disease"):
-
-        col1, col2 = st.beta_columns(2)
-
-        if data_type == "Image":
-
-            if input_data:
-                pred = detect_image(input_data, backend)
-                original_image = Image.open(input_data).convert("RGB")
-                converted_image = pred.content
-                converted_image = Image.open(io.BytesIO(converted_image)).convert("RGB")
-                r, g, b = converted_image.split()
-                converted_image = Image.merge("RGB", (b, g, r))
-
-                col1.header("Original")
-                col1.image(original_image, use_column_width=True)
-                col2.header("Detected")
-                col2.image(converted_image, use_column_width=True)
-
-            else:
-                # handle case with no image
-                st.write("Insert an image!")
-
-        elif data_type == "Video":
-
-            temp_path = "/var/lib/assets"
-            for t in os.listdir(temp_path):
-                os.remove(temp_path + "/" + t)
-
-            origin_video = input_data.read()
-
-            video_path = "/var/lib/assets/video1.mp4"
-            if os.path.isfile(video_path):
-                os.remove(video_path)
-
-            with open(video_path, "wb") as wfile:
-                wfile.write(origin_video)
-                logging.info(f"{video_path} added")
-
-            time.sleep(1)
-            wfile.close()
-            detect_video(backend)
-
-            time.sleep(1)
-
-            status = None
-            bar = st.progress(0)
-            # with stqdm(total=1, st_container=st) as pbar:
-            while status != "Success":
-                resp = get_video_status(backend)
-                resp_dict = json.loads(resp.content.decode("utf-8"))
-                status = resp_dict["status"]
-                if status != "Pending":
-                    progress = resp_dict["progress"]
-                    # pbar.update(int(progress))
-                    bar.progress(int(progress))
-
-                time.sleep(1)
-
-            time.sleep(3)
-
-            save_path = "/var/lib/assets/detect1.mp4"
-            convert_path = "/var/lib/assets/detect2.mp4"
-            os.system(f"ffmpeg -i {save_path} -vcodec libx264 {convert_path}")
-
-            video_file = open(convert_path, "rb")
-            video_bytes = video_file.read()
-
-            col1.header("Original")
-            col2.header("Detected")
-            col1.video(origin_video, format="video/mp4")
-            col2.video(video_bytes, format="video/mp4")
 
 
 if __name__ == "__main__":
--- backend/server.py
+++ backend/server.py
@@ -1,17 +1,17 @@
-from fastapi import FastAPI, File, BackgroundTasks
+from fastapi import FastAPI, File, BackgroundTasks, UploadFile
 from fastapi.logger import logger
 from fastapi.responses import Response
 
 import cv2
-import asyncio
 from models.experimental import attempt_load
 from utils.torch_utils import select_device
 from utils.general import non_max_suppression
 from utils.torch_utils import select_device
-from utils.prototype import drawBoxes, lookup_checkpoint_files, np_to_tensor
-from pydantic import BaseModel
+from utils.prototype import drawBoxes, np_to_tensor
 
+import time
 import io
+import os
 import numpy as np
 from PIL import Image
 
@@ -29,7 +29,6 @@ task = {}
 
 @app.post("/detection/image/")
 def post_predict_detector_image(file: bytes = File(...), width: int = 1280, height: int = 960, conf: float = 0.7, ckpt_file: str = 'yolor-d6.pt'):
-    print(width, height, conf, ckpt_file)
     logger.info("get image")
     image = Image.open(io.BytesIO(file))
     cv_image = np.array(image)
@@ -43,19 +42,54 @@ def post_predict_detector_image(file: bytes = File(...), width: int = 1280, heig
     converted_img.save(bytes_io, format="PNG")
     return Response(bytes_io.getvalue(), media_type="image/png")
 
-@app.post("/detection/video")
-async def post_predict_detector_video(background_task: BackgroundTasks):
+   
+@app.post("/detection/video/")
+async def post_predict_detector_video(file: UploadFile = File(...), width: int = 1280, height: int = 960, conf: float = 0.7, ckpt_file: str = 'yolor-d6.pt'):
     logger.info(f"Post Succes Video")
-    name = f"detect.mp4"
+    name = f"result.mp4"
     logger.info(f"file: {name}")
+    cap = cv2.VideoCapture(file.filename)
+    print(cap)
+    try:
+        num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        fps = int(cap.get(cv2.CAP_PROP_FPS)) 
+        print('Total number of frames to be processed:', num_frames,
+        '\nFrame rate (frames per second):', fps)
+    except:
+        print('We cannot determine number of frames and FPS!')
+    frame_counter = 0
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v') # output video codec
+    # Warning: 마지막 파라미터(이미지 크기 예:(1280, 960))가 안 맞으면 동영상이 저장이 안 됨!
+    video_writer = cv2.VideoWriter("/opt/ml/final_project/backend/result.mp4", fourcc, fps, (width, height))
+    start = time.time()
+    while cap.isOpened():
+        # if frame is read correctly ret is True
+        ret, frame = cap.read()
+        try:
+            frame = cv2.resize(frame, (width, height))
+        except: 
+            print('resize failed :', frame_counter)
+            if frame_counter/num_frames == 1:
+                break
+            continue
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+        frame_tensor = np_to_tensor(frame, device)
+        pred = detector_model(frame_tensor)[0]
+        pred = non_max_suppression(pred)[0]
+        frame = drawBoxes(frame, pred, conf) 
+
+        end = time.time()
+
+        frame_counter += 1
+        fps_measurement = frame_counter/(end - start)
+        print(f'Frames per second: {fps_measurement:.2f}')
+        print(frame_counter/num_frames)        
+        video_writer.write(frame)
+    video_writer.release()    
+    print('finish!')
     
-    video_path = "???"
-    cap = cv2.VideoCapture(video_path)
-    
-    background_task.add_task(
-        detector_model.detect, cap, image_size=416, video=True, save_path=name
-    )
-    
+
 @app.get("/detection/video/status")
 async def get_predict_detector_video():
     status, progress, save_path = detector_model.get_status()
