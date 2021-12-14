f158650a4360c51bfd5933272352601a6728ef00
Sung Dong Kim <ththth663@gmail.com>
2021-12-14 05:00:29 +0000

    Create : Create frontend folder.(Copied from /backend)

---
+++ frontend/frontend.py
@@ -0,0 +1,158 @@
+from turtle import back, width
+import streamlit as st
+
+# from stqdm import stqdm
+
+import io
+from PIL import Image
+import requests
+import time
+import tempfile
+import logging
+import json
+import os
+import sys
+sys.path.append(os.getcwd())
+import utils.SessionState as SessionState
+from random import randint
+from utils.prototype import lookup_checkpoint_files
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+from streamlit.server.server import Server
+
+backend = "http://localhost:8000"
+
+
+@st.cache(
+    hash_funcs={
+        st.delta_generator.DeltaGenerator: lambda x: None,
+        "_regex.Pattern": lambda x: None,
+    },
+    allow_output_mutation=True,
+)
+
+
+def trigger_rerun():
+    """
+    mechanism in place to force resets and update widget states
+    """
+    session_infos = Server.get_current()._session_info_by_id.values() 
+    for session_info in session_infos:
+        this_session = session_info.session
+    # this_session.request_rerun()
+    st.experimental_rerun()
+
+
+def detect_image(data, server, width, height, confthres, ckpt_file): # conf -> confthres
+    m = MultipartEncoder(fields={"file": ("filename", data, "image/jpeg")})
+    print(m)
+    resp = requests.post(
+        server + f"/detection/image/?width={width}&height={height}&confthres={confthres}&ckpt_file={ckpt_file}",
+        data=m,
+        headers={"Content-Type": m.content_type},
+        timeout=8000,
+    )
+
+    return resp
+
+     
+def detect_video(data, server, width, height, confthres, ckpt_file): # conf -> confthres
+    m = MultipartEncoder(fields={"file": ("filename", data, "video/mp4")})
+    requests.post(
+        server + f"/detection/video/?width={width}&height={height}&confthres={confthres}&ckpt_file={ckpt_file}",
+        data = m,
+        headers = {"Content-Type": m.content_type},
+        timeout=8000,
+    )
+
+
+def get_video_status(server):
+    resp = requests.get(
+        server + "/detection/video/status",
+        timeout=8000,
+    )
+
+    return resp
+
+
+def main():
+    st.set_page_config(page_title = "안전모 미착용, 승차인원 초과 멈춰~!", 
+    page_icon=":scooter:")
+
+    state = SessionState.get(upload_key = None, enabled = True, start = False, conf = 70, nms = 50, run = False)
+    data_type = st.selectbox("Choose Data Type", ["Image", "Video"])
+    upload = st.empty()
+    start_button = st.empty()
+    stop_button = st.empty()
+
+
+    with upload:
+        input_data = st.file_uploader('Upload Image or Video file', key = state.upload_key)
+        time.sleep(1)
+        
+        ckpt_files = lookup_checkpoint_files()
+
+        ckpt_file = st.sidebar.radio(
+            "select checkpoint file",
+            ckpt_files
+        )
+
+        confidence_threshold = st.sidebar.slider("Confidence score threshold", 
+            min_value=0.1,
+            max_value=1.0,
+            value=0.7,
+            step=0.05,
+            )
+        
+        result_resolution = st.sidebar.radio(
+            "select result video resolution",
+            ("1280 x 960", "640 x 480")
+        )
+
+        if result_resolution == "1280 x 960":
+            width, height = 1280, 960
+            
+        elif result_resolution == "640 x 480":
+            width, height = 640, 480
+    
+    filepath = '/opt/ml/final_project/web/result.mp4'
+    filepath_h264 = '/opt/ml/final_project/web/result_264.mp4'
+
+    if input_data is not None:
+        
+        if not state.run:
+            start = start_button.button("start")
+            state.start = start
+
+        if state.start:
+            start_button.empty()
+            state.enabled = False
+            
+            if state.run:
+                state.upload_key = str(randint(1000, int(1e6)))
+                state.enabled = True
+                state.run = False
+                
+                if os.path.exists(filepath):
+                    os.remove(filepath)
+                if os.path.exists(filepath_h264):
+                    os.remove(filepath_h264)
+
+                if data_type == "Image":
+                    resp = detect_image(input_data, backend, width, height, confidence_threshold, ckpt_file)
+                    st.image(resp.content)
+                    
+                elif data_type == "Video":
+                    detect_video(input_data, backend, width, height, confidence_threshold, ckpt_file)
+                    with st.spinner(text="Detecting Finished! Converting Video Codec..."):
+                        os.system("ffmpeg -i /opt/ml/final_project/backend/result.mp4 -vcodec libx264 /opt/ml/final_project/backend/result_h264.mp4")
+                        video_file = open("/opt/ml/final_project/backend/result_h264.mp4", 'rb')
+                        video_bytes = video_file.read()
+                        st.video(video_bytes)
+                
+            else:
+                state.run = True
+                trigger_rerun()
+
+
+if __name__ == "__main__":
+    main()
---
+++ frontend/utils/prototype.py
@@ -0,0 +1,69 @@
+import os
+import cv2
+import numpy as np
+import torch
+
+
+def DetermineBoxCenter(box):
+    cx = int(box[0] + (box[2]/2))
+    cy = int(box[1] + (box[3]/2))
+
+    return [cx, cy] 
+
+
+def drawBoxes(frame, pred, thres = 0.2): # thres 조절 추가 예정
+    pred = pred.to('cpu')
+    boxColor = {
+        0: (128, 255, 0), # 헬멧O 혼자O 초록색
+        1: (255, 255, 0), # 헬멧X 혼자O 하늘색
+        2: (0, 0, 255), # 헬멧O 혼자X 빨간색
+        3: (255, 0, 0), # 헬멧X 혼자X 파란색
+    }
+    className = {
+        0: "Helmet",
+        1: "NoHelmet",
+        2: "SharingHelmet",
+        3: "Sharing",
+    }
+    TextColor = (255, 255, 255) # white
+    boxThickness = 3 
+
+    for x1, y1, x2, y2, conf, lbl in pred:
+        if conf < thres:
+            break
+        lbl = int(lbl)
+        if lbl not in [0,1,2,3]:
+            continue
+        x1, y1, x2, y2, conf = int(x1), int(y1), int(x2), int(y2), float(conf) # tensor to int or float
+        start_coord = (x1, y1)
+        end_coord = (x2, y2)
+        # text to be included to the output image
+        txt = f'{className[lbl]} ({round(conf, 3)})'
+        frame = cv2.rectangle(frame, start_coord, end_coord, boxColor[lbl], boxThickness)
+        frame = cv2.putText(frame, txt, start_coord, cv2.FONT_HERSHEY_SIMPLEX, 0.5, TextColor, 2)
+
+    return frame
+    
+
+def lookup_checkpoint_files():
+
+    flie_list = list(os.listdir('/opt/ml/final_project/web/'))
+    flie_list.sort()
+    checkpoint_flie_list = []
+    for file in flie_list:
+        if file[-3:] == '.pt':
+            checkpoint_flie_list.append(file)
+
+    return tuple(checkpoint_flie_list)
+
+
+def np_to_tensor(image, device):
+    
+    image_tensor = np.transpose(image, (2, 0, 1))
+    image_tensor = torch.from_numpy(image_tensor).to(device)
+    image_tensor = image_tensor.float()  # uint8 to fp16/32
+    image_tensor /= 255.0  # 0 - 255 to 0.0 - 1.0
+    if image_tensor.ndimension() == 3:
+        image_tensor = image_tensor.unsqueeze(0)
+
+    return image_tensor
