287dded8490cb157fb1afbd6f24ff23867515043
bagineer <bih198721@gmail.com>
2021-12-19 18:53:15 +0900

    Refactor: Discard unused packages

--- app/CameraActivity.java
+++ app/CameraActivity.java
@@ -5,12 +5,9 @@ import android.content.Intent;
 import android.hardware.Camera;
 import android.media.MediaScannerConnection;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedOutputStream;
@@ -24,9 +21,6 @@ import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.nio.channels.FileChannel;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Set;
 
 import static android.os.Environment.getExternalStorageDirectory;
 
